6cbc3c620a790d866476c2f564215e0684f33bf7
Sascha Steffen <steffensascha@gmail.com>
2013-07-13 15:41:54 +0200

    Screenshots aktualisiert für Variante mit den straight edges

--- RDFtoyEd/project/src/Rdf2yed.java
+++ RDFtoyEd/project/src/Rdf2yed.java
@@ -2,7 +2,7 @@ public class Rdf2yed {
 
 	
 	public static void main(String[] args)  {
-		rdf2yed("res/raus.rdf", "res/out.graphml", "A3AOUEE6WH");
+		rdf2yed("res/raus.rdf", "res/out.graphml", "A3AOUEE6WH");//A3AOUEE6WH
 	}
 	
 	public static void rdf2yed (String inFile, String outFile, String equipment_uid_Query)
